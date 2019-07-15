95ba1d985b85bb18268c322b2a6fe0ea9638e8fa
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-15 15:33:06 +0200

    feat: button off

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ const savetext = document.querySelector('.save--js');
 const loadtext = document.querySelector('.load--js');
 const textarea = document.querySelector('.text--js');   
 
-savetext.addEventListener("click", function() {
+savetext.addEventListener("click", (e)=> {
+    e.preventDefault();
     localStorage.setItem('text', textarea.value);
 });
 
-loadtext.addEventListener("click", function() {
+loadtext.addEventListener("click", (e)=> {
+    e.preventDefault();
     textarea.value = localStorage.getItem('text');
 });
 
